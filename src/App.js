d6f24cd24a5280770e43eb18398aecdfda63ca05
Abhishek pandey <abhinavc222444c@gmail.com>
2023-05-18 23:19:55 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Abhishek from './components/expenseitem.js';
 
 function App() {
+   let expenseDate = new Date(2023 , 10 ,10);
+   let expenseTitle = 'School fee';
+   let expenseAmount = 200;
    return (
-
-    
-      <Abhishek></Abhishek> 
-    );
+      <div>
+         <h2>Lets start</h2>
+      <Abhishek
+      Date = {expenseDate}
+      Title = {expenseTitle}
+      Amount ={expenseAmount}>
+      
+      </Abhishek> 
+      </div>
+        );
 }
 
 export default App;
--- src/components/expenseitem.js
+++ src/components/expenseitem.js
@@ -1,15 +1,15 @@
 import './expenseitem.css';
 
-function Abhishek (){       //function name should be start with cap lat
+function Abhishek (props){       //function name should be start with cap lat
     return(
         <div className='expenseitem'> 
-             <div>March 12th 2022</div>
+             <div>{props.Date.toISOString()}</div>
                <div className='expenseitem_descr'>
-                <h2>car insurence</h2>
-                    <div className='expenseitem_price'>$200</div>
+                <h2>{props.Title}</h2>
+                    <div className='expenseitem_price'>${props.Amount}</div>
             </div>
         </div>
         )
     };  
-    
+
 export default Abhishek ;
